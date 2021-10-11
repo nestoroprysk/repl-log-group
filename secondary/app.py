8faceae9720a1fea475af86bb2655de9f02bc9fe
Serhii Abarovskyi <aserhii@protonmail.com>
2021-10-11 16:58:16 +0300

    Add secondary skeleton

---
+++ secondary/app.py
@@ -0,0 +1,21 @@
+import json
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+data = []
+
+
+@app.route("/messages", methods=["GET", "POST"])
+def get_msgs():
+    if request.method == "POST":
+        msg = json.loads(request.data)
+        data.append(msg)
+        return jsonify(msg)
+    return jsonify(data)
+
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "OK"
