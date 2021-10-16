6bb64d7c941ea12a4ffcbcdaaf6e2e80a0a95ac1
Alexander Buyko <aa.buyko@gmail.com>
2021-10-16 20:28:41 +0300

    implement async message replication

--- master/server.py
+++ master/server.py
@@ -1,35 +1,39 @@
+import os
+from threading import Thread, Lock
 from collections import deque
+from typing import Optional
 
-import requests, os
+import requests
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 messages = deque()
 
 client_ports = [os.getenv('SECONDARY_1_PORT'), os.getenv('SECONDARY_2_PORT')]
+lock = Lock()
 
 
 @app.route('/ping', methods=['GET'])
 def ping():
-    return 'ping'
+    return 'pong'
 
 
 @app.route('/messages', methods=['POST'])
 def add_message():
-    message = request.json.get('message')
+    with lock:
+        message = request.json.get('message')
+        messages.append(message)
 
-    messages.append(message)
-
-    # TODO: Do it in parallel
+    threads = list()
+    data = request.get_json()
     for index, port in enumerate(client_ports):
-        url = f'http://secondary-{index + 1}:{port}/messages'
-        r = requests.post(
-            url,
-            json=request.get_json(),
-        )
 
-        if r.status_code is not 200:
-            return f'{r.status_code}: {r.reason}'
+        thread = Thread(target=replicate_message, args=(data, index, port))
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
 
     return jsonify(message)
 
@@ -37,3 +41,14 @@ def add_message():
 @app.route('/messages', methods=['GET'])
 def list_messages():
     return jsonify(list(messages))
+
+
+def replicate_message(data: dict, index: int, port: str, ) -> Optional[str]:
+    url = f'http://secondary-{index + 1}:{port}/messages'
+    r = requests.post(
+        url,
+        json=data,
+    )
+
+    if r.status_code is not 200:
+        return f'{r.status_code}: {r.reason}'
