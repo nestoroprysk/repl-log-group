22ee7c968f78c7c5b3c8a6541a18e9f372a829e1
Serhii Abarovskyi <aserhii@protonmail.com>
2021-10-16 11:19:33 +0300

    Add delay and write lock (#3)
    - Delay in seconds
    - Lock for POST /messages

--- secondary/app.py
+++ secondary/app.py
@@ -1,17 +1,26 @@
-import json
+from threading import Lock
+from time import sleep
 
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 data = []
+lock = Lock()
 
 
 @app.route("/messages", methods=["GET", "POST"])
 def get_msgs():
     if request.method == "POST":
-        msg = request.json.get('message')
+        lock.acquire()
+        msg = request.json.get("message")
+        delay = request.json.get("delay")
+
+        if delay:
+            sleep(float(delay))
+
         data.append(msg)
+        lock.release()
         return jsonify(msg)
     return jsonify(data)
 
